e5fbc81be1897f85c30ceeabb6cde2c1be4a5cc0
nganoro <nganoro1@umbc.edu>
2025-03-31 21:29:48 -0400

    first commit

---
+++ src/test/java/dev/natgan/HelloLambdaTest.java
@@ -0,0 +1,23 @@
+package dev.natgan;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class HelloLambdaTest {
+
+    @Test
+    public void successfulResponse() {
+        HelloLambda app = new HelloLambda();
+        APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+        assertEquals(200, result.getStatusCode().intValue());
+        assertEquals("application/json", result.getHeaders().get("Content-Type"));
+        String content = result.getBody();
+        assertNotNull(content);
+        assertTrue(content.contains("\"message\""));
+        assertTrue(content.contains("\"hello world\""));
+        assertTrue(content.contains("\"location\""));
+    }
+}
